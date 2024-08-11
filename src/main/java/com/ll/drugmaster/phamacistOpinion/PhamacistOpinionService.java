80d2c6085e2affc10c0fbb80e6194c8592467d31
seyeong <seyoung4503@gmail.com>
2024-08-12 00:20:22 +0900

    feat : Phamacist Opinion service 추가

---
+++ src/main/java/com/ll/drugmaster/phamacistOpinion/PhamacistOpinionService.java
@@ -0,0 +1,13 @@
+package com.ll.drugmaster.phamacistOpinion;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class PhamacistOpinionService {
+
+    public final PhamacistOpinionRepository phamacistOpinionRepository;
+
+
+}
